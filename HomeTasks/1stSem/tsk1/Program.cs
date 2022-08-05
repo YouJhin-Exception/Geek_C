733c81ea0a785c5a038753ed42f544bffad066c3
«YouJhin» <palevich86@mail.ru>
2022-08-05 16:48:52 +0300

    add 1st task

--- HomeTasks/1stSem/tsk1/Program.cs
+++ HomeTasks/1stSem/tsk1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число - ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1>n2)
+{
+ Console.WriteLine("число " +n1+ " больше чем "+n2);   
+}else{
+    Console.WriteLine("число " +n2+ " больше чем "+n1);
+}
