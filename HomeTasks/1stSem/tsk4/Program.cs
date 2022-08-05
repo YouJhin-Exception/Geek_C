45d6bdec1648c64c2f5036d46f4bfd1ae785a64f
«YouJhin» <palevich86@mail.ru>
2022-08-05 17:35:47 +0300

    add 4st task

---
+++ HomeTasks/1stSem/tsk4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число для определения диапазона N - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n1; i++){
+    if (i % 2 == 0){
+        Console.Write(i + " ");
+    }     
+}
