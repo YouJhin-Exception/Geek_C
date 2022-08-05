926cfc029b58d5de405e1c8f05255581fc756f17
«YouJhin» <palevich86@mail.ru>
2022-08-05 16:58:55 +0300

    add 2st task

---
+++ HomeTasks/1stSem/tsk2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число - ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число - ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(n1, (Math.Max(n2,n3)));
+Console.WriteLine("Максимальное число из этих трех чисел это "+max);
+
