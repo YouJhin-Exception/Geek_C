3f13cdd0f2b428582d9289bc7063a39625ce705e
«YouJhin» <palevich86@mail.ru>
2022-08-05 17:10:40 +0300

    add 3st task

---
+++ HomeTasks/1stSem/tsk3/Program.cs
@@ -0,0 +1,5 @@
+﻿// через тернарник проще всего
+
+Console.WriteLine("Введите число для проверки на четность - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n1 % 2 == 0 ? "Четное" : "Не четное");
