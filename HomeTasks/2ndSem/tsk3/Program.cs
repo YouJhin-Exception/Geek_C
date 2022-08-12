d38d1afd6a2eb3bd30e55b9dcaa4630a1ea6ad66
«YouJhin» <palevich86@mail.ru>
2022-08-12 12:29:12 +0300

    add s2t3

---
+++ HomeTasks/2ndSem/tsk3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1<1 || n1>7)
+    Console.WriteLine("Странная у вас неделя...");
+else if (n1 == 6 || n1 == 7)  
+    Console.WriteLine("Это выходной день");
+else
+    Console.WriteLine("Это рабочий день");
