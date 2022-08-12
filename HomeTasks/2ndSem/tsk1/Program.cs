1333d3a7f1fa43abaf6ce5e8408a48982717abad
«YouJhin» <palevich86@mail.ru>
2022-08-12 11:48:22 +0300

    add s2t1

---
+++ HomeTasks/2ndSem/tsk1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите трехзначное число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if ((n1 / 100 > 0) && (n1 / 1000 == 0))
+ {
+  int n2 = (n1/10)%10;
+  Console.WriteLine(n2);
+ }
+else
+Console.WriteLine("Число НЕ трехзначное");
+
+
+
+
+
+
+
+
+
+
