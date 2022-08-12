099b3b337dcae48cb00ec2ffb0618c42ff3bcbdd
«YouJhin» <palevich86@mail.ru>
2022-08-12 15:34:53 +0300

    add in 2s2t second var

--- HomeTasks/2ndSem/tsk2/Program.cs
+++ HomeTasks/2ndSem/tsk2/Program.cs
@@ -8,3 +8,13 @@ if(n1str.Length>2)
 }
 else
 Console.WriteLine("третьей цифры нет");
+
+
+
+// без стринга 
+
+// Console.Write("Введите число - ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// while(n1>1000)
+//  n1 = n1/10;
+// Console.WriteLine(n1%10);
