769634bb4403c0a49bdd54192cf4139488a6b0ae
«YouJhin» <palevich86@mail.ru>
2022-08-12 12:16:16 +0300

    add s2t2

---
+++ HomeTasks/2ndSem/tsk2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+string n1str = Convert.ToString(n1);
+
+if(n1str.Length>2)
+{
+   Console.WriteLine("третья цифра "+n1str[2]); 
+}
+else
+Console.WriteLine("третьей цифры нет");
