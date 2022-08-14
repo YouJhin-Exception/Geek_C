7520ee5884fe1d21a037a94e90b8df555cff7089
«YouJhin» <palevich86@mail.ru>
2022-08-14 14:06:34 +0300

    add 3st3

---
+++ HomeTasks/3dSem/tsk3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Cube (int n)
+{
+    Console.Write(n+" -> ");
+    int i = 1;
+    while (i<=n)
+    {
+        System.Console.Write(Math.Pow(i,3)+" ");
+        i++;
+    }
+    System.Console.WriteLine();
+    //System.Console.WriteLine(Math.Pow(n,3)+".");
+
+}
+Cube(3);
+Cube(5);
+Cube(7);
