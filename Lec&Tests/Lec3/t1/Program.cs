0df6f28cf8d4cd01851de35058d9ddbbc0126f43
«YouJhin» <palevich86@mail.ru>
2022-08-15 16:20:15 +0300

    add lec3t1

---
+++ Lec&Tests/Lec3/t1/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray (int[] array)
+{
+    int count = array.Length; 
+    for (int i = 0; i < count; i++)
+    {
+       Console.Write(array[i]+" "); 
+    }
+}
+
+
+
+
+void MinSort(int[] mass )
+{
+    for (int i = 0; i < mass.Length - 1; i++)
+    {
+        int minPos = i;
+        for (int j = i+1 ; j < mass.Length; j++)
+        {
+            if (mass[j]<mass[minPos])                     // от большего >
+            {
+                minPos = j;
+            } 
+        }
+        int temp = mass[i];
+        mass[i] = mass[minPos];
+        mass[minPos] = temp;
+
+    }
+}
+int[] array = {2,4,7,8,5,3,1,0,9,0};
+PrintArray(array);
+MinSort(array);
+Console.WriteLine();
+PrintArray(array);
